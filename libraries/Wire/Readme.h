721316c985753a6f2905f6a516bd8e9e7df88ef4
Robert Wessels <robertinant@yahoo.com>
2016-06-29 11:39:01 -0700

    Add dummy header files to SPI/Wire.
    This file is here to prevent the IDE complaining about this library being invalid.
    The SPI/Wire implementation for all EMT targets is common and hence live in
    cores/msp432/ti/runtime/wiring/

---
+++ libraries/Wire/Readme.h
@@ -0,0 +1,5 @@
+/*
+ * This file is here to prevent the IDE complaining about this library being invalid.
+ * The Wire implementation for all EMT targets is common and hence live in
+ * cores/msp432/ti/runtime/wiring/
+ */
